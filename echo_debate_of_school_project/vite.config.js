4aeed984464e042908027f6b7c9429f23610d39a
pinnpen <aa0983563272@gmail.com>
2025-09-13 01:49:43 +0800

    沒有正常輸出dist artifact，所以跑 回去内建 Jekyll

--- echo_debate_of_school_project/vite.config.js
+++ echo_debate_of_school_project/vite.config.js
@@ -2,21 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/echo_debate_of_school_project/',  // 倉庫名，前后都要有斜线
+  base: '/echo_debate_of_school_project/',
   plugins: [react()],
   build: {
-    sourcemap: true,  // 加這個方便你找出錯誤
-  },
+    sourcemap: true
+  }
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default { 
-//   base: '/echo_debate_of_school_project/' , //  倉庫名
-//   plugins: [react()],
-// }
-// // export default defineConfig({
-// //   plugins: [react()], 
-// // })
