4f6a3207196dc116a15b67c6d7125d19cd44b3fd
pinnpen <aa0983563272@gmail.com>
2025-09-13 01:37:38 +0800

    修改vite看錯誤資訊

--- echo_debate_of_school_project/vite.config.js
+++ echo_debate_of_school_project/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default { 
-  base: '/echo_debate_of_school_project/' , //  倉庫名
+export default defineConfig({
+  base: '/echo_debate_of_school_project/',  // 倉庫名，前后都要有斜线
   plugins: [react()],
-}
-// export default defineConfig({
-//   plugins: [react()], 
-// })
+  build: {
+    sourcemap: true,  // 加這個方便你找出錯誤
+  },
+})
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default { 
+//   base: '/echo_debate_of_school_project/' , //  倉庫名
+//   plugins: [react()],
+// }
+// // export default defineConfig({
+// //   plugins: [react()], 
+// // })
